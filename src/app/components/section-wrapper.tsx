6ccfdfd9f04bc57fc585521ec286b52571fdcca5
Ole Magnus Fon Johnsen <me@omfj.no>
2025-07-16 15:34:36 +0200

    Make section wrapper a section

--- src/app/components/section-wrapper.tsx
+++ src/app/components/section-wrapper.tsx
@@ -2,6 +2,8 @@ import { PropsWithChildren } from "react";
 
 export default function SectionWrapper(props: PropsWithChildren) {
   return (
-    <div className="w-[65vw] max-w-[1400px] m-auto py-2">{props.children}</div>
+    <section className="w-[65vw] max-w-[1400px] m-auto py-2">
+      {props.children}
+    </section>
   );
 }
