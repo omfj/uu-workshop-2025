9939910434abb4b4ca0d47ab2f4b32bd1a65ee5d
Ole Magnus Fon Johnsen <me@omfj.no>
2025-07-16 15:24:33 +0200

    Change alt text

--- src/app/components/marketplace-posting.tsx
+++ src/app/components/marketplace-posting.tsx
@@ -13,7 +13,7 @@ const MarketplacePosting: React.FC<TMarketplacePosting> = ({
     <li className="border border-gray-200 rounded-lg p-4 max-w-xs shadow-sm bg-white">
       <img
         src={imageUrl}
-        alt={description}
+        alt={`Image of ${title} posting`}
         className="w-[300px] h-[300px] object-cover rounded-md"
       />
 
@@ -21,7 +21,7 @@ const MarketplacePosting: React.FC<TMarketplacePosting> = ({
       <p className="text-gray-600 mb-2">{description}</p>
 
       <div className="flex justify-between items-center">
-        <span className="font-bold text-base text-[#aaa]">{price}</span>
+        <span className="font-bold text-base text-gray-400">{price}</span>
         <span className="text-gray-400 text-sm">{date.toLocaleString()}</span>
       </div>
     </li>
