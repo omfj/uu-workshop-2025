f648adb82cc605f72b63ea9d97c4acc6d497bba1
Ole Magnus Fon Johnsen <me@omfj.no>
2025-07-16 15:29:00 +0200

    Format date

--- src/app/components/marketplace-posting.tsx
+++ src/app/components/marketplace-posting.tsx
@@ -2,6 +2,16 @@
 import React from "react";
 import { TMarketplacePosting } from "@/app/db/db-crud";
 
+const formatDate = (date: Date | string) => {
+  const d = new Date(date);
+
+  return d.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
 const MarketplacePosting: React.FC<TMarketplacePosting> = ({
   imageUrl,
   title,
@@ -22,7 +32,7 @@ const MarketplacePosting: React.FC<TMarketplacePosting> = ({
 
       <div className="flex justify-between items-center">
         <span className="font-bold text-base text-gray-400">{price} USD</span>
-        <span className="text-gray-400 text-sm">{date.toLocaleString()}</span>
+        <span className="text-gray-400 text-sm">{formatDate(date)}</span>
       </div>
     </li>
   );
